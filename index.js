d72240316399cfdb4288c1cffd67439001fba331
Sajid <sajidmohammad460519@gmail.com>
2024-06-22 17:05:07 +0800

    able to signup users, however, i think the code is kinda iffy, i'll have to learn more express js and stuff to understand what's going on

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const UserModel = require('./models/Users')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/islamic-series") //this is the database
+
+app.post('/register', (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
